667927bf0540e87bf30f5af118c11c27a00907b0
Tommy840602 <tommy840602@gmail.com>
2024-05-29 17:46:59 +0800

    Add files via upload

---
+++ server.py
@@ -0,0 +1,23 @@
+import socket
+
+HOST = '127.0.0.1'
+PORT = 9999
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(5)
+
+print('server start at: %s:%s' % (HOST, PORT))
+print('wait for connection...')
+
+while True:
+    conn, addr = s.accept()
+    print('connected by ' + str(addr))
+
+    indata = conn.recv(1024)
+    print('recv: ' + indata.decode())
+
+    outdata = 'echo ' + indata.decode()
+    conn.send(outdata.encode())
+    conn.close()
+s.close()
